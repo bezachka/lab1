2c30405d280cebbc51d7d01f892ad4aa852f8aa2
bezachka <ilyaershov135@gmail.com>
2025-10-19 15:06:28 +0300

    versin 0.2

--- Music_Service.py
+++ Music_Service.py
@@ -1,101 +1,204 @@
 import datetime
+import json
+
+
+class Device:
+    def __init__(self, name: str, os: str):
+        self.name = name
+        self.os = os
+
+    def set_device(self, name: str, os: str):
+        self.name = name
+        self.os = os
+
+    def to_dict(self):
+        return {"name": self.name,
+                "os" : self.os}
+
+
+class Album:
+    def __init__(self, tracks: list['Track'], title: str, year: int):
+        self.tracks = tracks
+        self.title = title
+        self.year = year
+
+    def add_track(self, *tracks: list['Track']):
+        for track in tracks:
+            self.tracks.append(track)
+        
+    def adel_track(self, *tracks: list['Track']):
+        for track in tracks:
+            self.tracks.remove(track)
+
+
+    def to_dict(self):
+        return {
+            "title": self.title,
+            "year": self.year,
+            "tracks": [track.title for track in self.tracks]
+        }
+
+
+class Rating:
+    def __init__(self, value: int):
+        self.value = value
+
+    def to_dict(self):
+        return {
+            "value": self.value
+        }
+    
+
+class Lyrics:
+    def __init__(self, text: str):
+        self.text = text
+
+    def to_dict(self):
+        return {"lyrics" : self.text}
+        
+      
+class Genre:
+    def __init__(self, type_genre: str):
+        self.type_genre = type_genre
+
+    def to_dict(self):
+        return {"type_genre": self.type_genre}
+
+
+class Track:
+    def __init__(self, title: str, duration: int, rating: Rating, genre = Genre("Жанр не указан"), lyrics = Lyrics("Субтитры не указаны")):
+        self.title = title
+        self.duration = duration
+        self.lyrics = lyrics
+        self.genre = genre
+        self.rating = rating
+        
+
+    def to_dict(self):
+        return {
+            "title": self.title,
+            "duration": self.duration,
+            "lyrics" : self.lyrics.to_dict(),
+            "genre" : self.genre.to_dict(),
+            "rating" : self.rating.to_dict()
+        }
+
+
+class Playlist:
+    def __init__(self, tracks: list['Track'], title: str, is_public = True):
+        self.tracks = tracks
+        self.title = title
+        self.is_public = is_public
+
+    def show_tracks(self):
+        return [track for track in self.tracks]
+        
+    def set_visibility(self, visibility: bool):
+        self.is_public = visibility
+
+    def add_tracks(self, *tracks: list['Track']):
+        for track in tracks:
+            self.tracks.append(track)
+
+    def delete_tracks(self, *tracks: list['Track']):
+        for track in tracks:
+            self.tracks.remove(track)
+
+    def to_dict(self):
+        return {
+            "title": self.title,
+            "is_public": self.is_public,
+            "tracks": [track.to_dict() for track in self.tracks]
+        }
+
 
 class User:
-    def __init__(self, username, email):
+    def __init__(self, username: str, email: str):
         self.username = username
         self.email = email
         self.playlists = []
         self.liked_tracks = []
         self.fav_artists = []
-        self.history = History(self)
+        self.device = Device(name="PC", os="Windows 11")
 
-    def __str__(self):
-        return f"{self.username}"
+    def to_dict(self):
+        return {
+            "username" : self.username,
+            "email" : self.email,
+            "playlists" : [playlist.to_dict() for playlist in self.playlists if self.playlists],
+            "liked_tracks" : [track.to_dict() for track in self.liked_tracks if self.liked_tracks],
+            "fav_artists" : [{"username" : art.username, "bio" : art.bio} for art in self.fav_artists if self.fav_artists],
+            "device" : self.device.to_dict()
+        }
+        
+    def like_track(self, *tracks: list['Track']):
+        for track in tracks:
+            self.liked_tracks.append(track)
 
+    def delete_track(self, *tracks: list['Track']):
+        for track in tracks:
+            self.liked_tracks.remove(track)
+
+    def list_of_fav_track(self):
+        return [track for track in self.liked_tracks if self.liked_tracks]
+    
+    def add_playlists(self, *playlists: list["Playlist"]):
+        for playlist in playlists:
+            self.playlists.append(playlist)
+
+    def delete_playlists(self, *playlists: list["Playlist"]):
+        for playlist in playlists:
+            self.playlists.remove(playlist)
+
+    def list_of_playlists(self):
+        return [playlist for playlist in self.playlists]
+
+    def add_fav_artisits(self, *artists: list['Artist']):
+        for art in artists:
+            self.fav_artists.append(art)
+
+    def del_fav_artisits(self, *artists: list['Artist']):
+        for art in artists:
+            self.fav_artists.remove(art)
+
+    def show_fav_artists(self):
+        return [art for art in self.fav_artists]
+    
         
 class Artist:
-    def __init__(self, username, bio):
+    def __init__(self, username: str, bio: str):
         self.username = username
         self.bio = bio
-
-    def __str__(self):
-        return self.username
-
-class Album:
-    def __init__(self, tracks: list, title, year, author):
-        self.tracks = tracks
-        self.title = title
-        self.year = year
-        self.author = author
-
-    def __str__(self):
-        return f"{self.title} - {self.author} [{self.year}]"
-
+        self.albums = []
+        self.tracks = []
         
-class Track:
-    def __init__(self, title, duration, author, genre, rating = None):
-        self.title = title
-        self.duration = duration
-        self.author = author
-        self.genre = genre
-        self.rating = rating
+    def to_dict(self):
+        return {
+            "username": self.username,
+            "bio": self.bio,
+            "albums" : [album.to_dict() for album in self.albums if self.albums],
+            "tracks" : [track.to_dict() for track in self.tracks if self.tracks],
+        }
 
-    def __str__(self):
+
+class Service:
+    def __init__(self, user = None, artist = None):
+        self.users = [user]
+        self.artists = [artist]
         
-        minutes = self.duration // 60
-        seconds = self.duration % 60
-        return f"{self.author} — {self.title} [{minutes}:{seconds:02d}]"
-
-
-class Playlist:
-    def __init__(self, tracks, title, author, is_public):
-        self.tracks = tracks
-        self.title = title
-        self.author = author
-        self.is_public = is_public
-
-    def __str__(self):
-        return f"{self.title} - {self.author}"
-
-    
-class Genre:
-    def __init__(self, type_genre):
-        self.type_genre = type_genre
-
-    def __str__(self):
-        return self.type_genre
-
-
-
-class History:
-    def __init__(self, user):
-        self.user = user
-        self.listened_track = []
-        self.last_track = []
-
-
-class Rating:
-    def __init___(self, user, track, value):
-        self.user = user
-        self.track = track
-        self.value = value
-
-
-class Lyrics:
-    def __init__(self, text, track):
-        self.text = text
-        self.track = track
-
-
-class Download:
-    def __init__(self, user, track):
-        self.user = user
-        self.track = track
-        self.date = datetime.now()
-    
-    def __str__(self):
-        return f"{self.track} скачен {self.user}"
-
+    def to_json(self, filename):
+        try:
+            data = {"music_service" : {"users" : [user.to_dict() for user in self.users if user is not None],
+                    "artists" : [artist.to_dict() for artist in self.artists if artist is not None]}}
+            
+            with open(filename, 'w', encoding='utf-8') as f:
+                json.dump(data, f, indent=2, ensure_ascii=False)
+            
+            print(f"Данные успешно сохранены в {filename}")
+        
+        except Exception as e:
+            print(f"Ошибка при сохранении в JSON: {e}")
+        
 
 if __name__ == "__main__":
     ...
--- main.py
+++ main.py
@@ -1,19 +1,22 @@
-from Music_Service import Artist, Genre, Track, User, Playlist
+from Music_Service import Artist, Genre, Track, User, Playlist, Service, Album, Rating
 
-author1 = Artist("Серега Пират", "Стример по Доте 2 и Артист")
+author1 = Artist("Artist1", "bio1")
+author2 = Artist("Artist2", "bio2")
 
 genre1 = Genre("Рок")
 
-track1 = Track("Я это я", 134, author1, genre1)
-track2 = Track("В этой траве", 72, author1, genre1)
+track1 = Track("Track1", 134, rating=Rating(5))
+track2 = Track("Track2", 72, rating=Rating(5))
+track3 = Track("Track3", 102, rating=Rating(5))
+track4 = Track("Track4", 252,  rating=Rating(5))
 
 user1 = User("beza", "lol@gmail.com")
 
-playlist1 = Playlist([track1, track2], "Имба", author1, True)
-user1.add_playlists(playlist1)
-user1.list_of_playlists()
-playlist1.lst_trk()
 
-user1.list_of_fav_track()
-user1.like_track(track2, track1)
-user1.list_of_fav_track()
+playlist1 = Playlist([track1, track2, track3, track4], "playlist1", is_public=True)
+user1.add_playlists(playlist1)
+user1.like_track(Track("Track5", 230, Rating(4)))
+user1.add_fav_artisits(Artist("Artist3", "bio3"))
+
+structure = Service(user = user1, artist= author1)
+structure.to_json("music_structure_1.json")
